3925df9444719f81263ed256d7335a785284abf1
mkt-droid <mahakalitantra151@gmail.com>
2026-02-26 17:20:25 +0530

    Updated source code to work with cpanel.

--- src/components/about/AboutCard.jsx
+++ src/components/about/AboutCard.jsx
@@ -7,7 +7,7 @@ const AboutCard= () => {
     return (
         <div className="h-120 w-100">
             <div className="rounded-lg overflow-hidden">
-                <img className="zoom-in" src="/img/maleFace.jpeg" />
+                <img className="zoom-in" src="./img/maleFace.jpeg" />
             </div>
             <h2 className="text-xl font-[500] mt-3 text-[#272A59]">
                 Name Surname
