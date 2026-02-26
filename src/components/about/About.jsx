a2b26396abfdffa952fae53e138696871c41a961
mkt-droid <mahakalitantra151@gmail.com>
2026-02-26 18:17:45 +0530

    Edited about page, added names and pics

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,6 +3,35 @@ import AboutCard from './AboutCard'
 
 
 const About = () => {
+
+    const keyPeople = [
+        {
+            name: "Manish Chanana",
+            img: "manish.jpg",
+            designation: "Co-Founder & CEO",
+            description:"Lorem Ipsum is simply dummy sample text of the printing and typesetting industry. Lorem Ipsum has been.",
+            email:"",
+            mobile:""
+        },
+        {
+            name: "Varun Dutt",
+            img: "varun.jpg",
+            designation: "Co-Founder & COO",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.",
+            email:"",
+            mobile:""
+        },
+        {
+            name: "Raj Chengappa",
+            img: "raj.jpg",
+            designation: "Co-Founder & CBO",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.",
+            email:"",
+            mobile:""
+        }
+
+    ]
+
     return (
         <div className="w-[calc(100%-6rem)]">
             <header className="bg-[#e5e6f8] p-3">
@@ -10,20 +39,20 @@ const About = () => {
             </header>
             <div className="Content">
                 <div className="flex flex-col items-center pt-12">
-                    <div className="text-xs text-[#272A59]">
+                    <div className="text-sm text-[#272A59]">
                         The Leadership
                     </div>
-                    <h1 className="text-7xl font-semibold uppercase text-[#272A59]">
+                    <h1 className="text-8xl font-semibold uppercase text-[#272A59]">
                         About Us
                     </h1>
                 </div>
-                <div className="flex justify-center m-12">
-                    <img src="./img/chevron.jpeg" alt="chevron" />
+                <div className="w-5/10 mx-auto text-center m-12 text-[32px] leading-10">
+                    <p>We bring together experience of 50+ man years and 100+programs in Rewards and Loyalty.</p>
                 </div>
-                <div className="container grid grid-rows-1 grid-cols-3 p-10 justify-items-center gap-x-5 w-fit mx-auto">
-                    <AboutCard />
-                    <AboutCard />
-                    <AboutCard />
+                <div className="container grid grid-rows-1 grid-cols-3 p-10 justify-items-center gap-x-10 w-fit mx-auto">
+                    <AboutCard person={keyPeople[0]} />
+                    <AboutCard person={keyPeople[1]} />
+                    <AboutCard person={keyPeople[2]} />
                 </div>
             </div>
             <img src="./img/faqs.jpg" alt="faq" />
--- src/components/about/AboutCard.jsx
+++ src/components/about/AboutCard.jsx
@@ -2,21 +2,23 @@ import './AboutCard.css';
 
 
 
-const AboutCard= () => {
+const AboutCard= (props) => {
+
+    const {person} = props;
 
     return (
-        <div className="h-120 w-100">
-            <div className="rounded-lg overflow-hidden">
-                <img className="zoom-in" src="./img/maleFace.jpeg" />
+        <div className="w-100">
+            <div className="rounded-2xl overflow-hidden">
+                <img className="zoom-in aspect-9/10" src={`./img/${person.img}`} />
             </div>
-            <h2 className="text-xl font-[500] mt-3 text-[#272A59]">
-                Name Surname
+            <h2 className="mt-6 text-4xl font-[500] mt-3 text-[#1c1f5d]">
+                {person.name}
             </h2>
-            <h3 className="text-md">
-                Designation
+            <h3 className="text-xl font-[500] my-3">
+                {person.designation}
             </h3>
-            <p className="text-xs mt-1 font-[300]">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.
+            <p className="text-[18px] mt-1 font-[400]">
+                {person.description}
             </p>
         </div>
     )
